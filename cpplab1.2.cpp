4b1061718eb892d447c9c1166c681c331d2814e0
maslorik <medyanik.danechka@mail.ru>
2023-03-14 22:52:32 +0300

    Добавил русский язык

--- cpplab1.2.cpp
+++ cpplab1.2.cpp
@@ -7,3 +7,12 @@
 #include <stdlib.h>
 
 using namespace std;
+int main()
+
+{
+	int getch();
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	const int N = 15;
+	double z = 0, a[N];
+	int x, j, c = 0;
